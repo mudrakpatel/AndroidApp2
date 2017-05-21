cca6c552a1b2599b128f3886d248c17e7f837bc2
mudrakpatel <mudrakpatel96@gmail.com>
2017-05-21 16:00:54 +0530

    Intent activity to and fro works; Toggle button works;

--- app/src/main/java/apps/mudrak/androidapp2/Activity2.java
+++ app/src/main/java/apps/mudrak/androidapp2/Activity2.java
@@ -4,16 +4,42 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.CompoundButton;
+import android.widget.TextView;
 import android.widget.Toast;
+import android.widget.ToggleButton;
 
 public class Activity2 extends AppCompatActivity {
+    //Required variables
+    ToggleButton toggleButton1;
+    TextView textView2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_2);
-    }
+        toggleButton1 = (ToggleButton) findViewById(R.id.toggleButton1);
 
+        //toggleButton1 OnClickListener code
+        toggleButton1.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener(){
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(isChecked){
+                    Toast.makeText(getApplicationContext(), "Toggled ON...\nBringing TextView...", Toast.LENGTH_SHORT).show();
+                    textView2 = (TextView) findViewById(R.id.textView2);
+                    textView2.setVisibility(View.VISIBLE);
+                } else {
+                    Toast.makeText(getApplicationContext(), "Toggled OFF...\nHiding TextView...", Toast.LENGTH_SHORT).show();
+                    textView2 = (TextView) findViewById(R.id.textView2);
+                    textView2.setVisibility(View.INVISIBLE);
+                }
+            }
+        });
+    }
+    /**
+     * @method textView2OnClick
+     * @param view
+     */
     public void textView2OnClick(View view){
         Toast.makeText(getApplicationContext(), "Starting new activity..." ,Toast.LENGTH_SHORT).show();
         startActivity(new Intent(getApplicationContext(), Activity1.class));
