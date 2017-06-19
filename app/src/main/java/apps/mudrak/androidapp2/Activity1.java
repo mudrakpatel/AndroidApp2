40c72114a2a185686ff718b76ec160c05c9bf5d7
mudrakpatel <mudrakpatel96@gmail.com>
2017-06-19 17:23:09 -0400

    Intent data passing works fine;

--- app/src/main/java/apps/mudrak/androidapp2/Activity1.java
+++ app/src/main/java/apps/mudrak/androidapp2/Activity1.java
@@ -4,18 +4,31 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class Activity1 extends AppCompatActivity {
+
+    TextView textView1;
+    Intent intent1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_1);
+        textView1 = (TextView) findViewById(R.id.textView1);
+        textView1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    intent1 = new Intent(getApplicationContext(), Activity2.class);
+                    intent1.putExtra("Message","Activity1 message:\n" + textView1.getText().toString());
+                    startActivity(intent1);
+                } catch(Exception exception){
+                    System.out.println("LOOK HERE!\n"+ exception.getMessage());
+                }
+            }
+        });
     }
 
-    public void textView1OnClick(View view){
-        Toast.makeText(getApplicationContext(), "Starting new activity..." ,Toast.LENGTH_SHORT).show();
-        startActivity(new Intent(getApplicationContext(), Activity2.class));
-    }
 }
--- app/src/main/java/apps/mudrak/androidapp2/Activity2.java
+++ app/src/main/java/apps/mudrak/androidapp2/Activity2.java
@@ -11,37 +11,37 @@ import android.widget.ToggleButton;
 
 public class Activity2 extends AppCompatActivity {
     //Required variables
-    ToggleButton toggleButton1;
+    /*ToggleButton toggleButton1;*/
     TextView textView2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_2);
-        toggleButton1 = (ToggleButton) findViewById(R.id.toggleButton1);
+        try {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_2);
+            /*toggleButton1 = (ToggleButton) findViewById(R.id.toggleButton1);*/
+            textView2 = (TextView) findViewById(R.id.textView2);
+            Intent intent = getIntent();
+            textView2.setText(intent.getStringExtra("Message"));
 
-        //toggleButton1 OnClickListener code
-        toggleButton1.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener(){
-            @Override
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                if(isChecked){
-                    Toast.makeText(getApplicationContext(), "Toggled ON...\nBringing TextView...", Toast.LENGTH_SHORT).show();
-                    textView2 = (TextView) findViewById(R.id.textView2);
-                    textView2.setVisibility(View.VISIBLE);
-                } else {
-                    Toast.makeText(getApplicationContext(), "Toggled OFF...\nHiding TextView...", Toast.LENGTH_SHORT).show();
-                    textView2 = (TextView) findViewById(R.id.textView2);
-                    textView2.setVisibility(View.INVISIBLE);
+/*            //toggleButton1 OnClickListener code
+            toggleButton1.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+                @Override
+                public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                    if (isChecked) {
+                        Toast.makeText(getApplicationContext(), "Toggled ON...\nBringing TextView...", Toast.LENGTH_SHORT).show();
+                        textView2 = (TextView) findViewById(R.id.textView2);
+                        textView2.setVisibility(View.VISIBLE);
+                        textView2.setText(importedData.getString("Message"));
+                    } else {
+                        Toast.makeText(getApplicationContext(), "Toggled OFF...\nHiding TextView...", Toast.LENGTH_SHORT).show();
+                        textView2 = (TextView) findViewById(R.id.textView2);
+                        textView2.setVisibility(View.INVISIBLE);
+                    }
                 }
-            }
-        });
-    }
-    /**
-     * @method textView2OnClick
-     * @param view
-     */
-    public void textView2OnClick(View view){
-        Toast.makeText(getApplicationContext(), "Starting new activity..." ,Toast.LENGTH_SHORT).show();
-        startActivity(new Intent(getApplicationContext(), Activity1.class));
+            });*/
+        } catch (Exception exception){
+            System.out.println("LOOK HERE!\n" + exception.getMessage());
+        }
     }
 }
